68566a36915aded6d95f7afceab57430f09d4895
bhagwatiprasad <bhagwatijoshi325@gmail.com>
2021-07-16 17:27:46 +0530

    first commit

---
+++ Competitive_programming.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+void test (){
+
+}
+int initialize(){
+    int i = -1;
+    vector<string> menu_list ;
+    // get list of all functions defined in the file
+    while(i != 0){
+
+    }
+}
+
+int main (){
+    initialise();
+    test();
+    return 0;
+}
